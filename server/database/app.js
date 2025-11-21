62d36349af6d8a72846d15b877228831803e09b0
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-11-21 13:33:12 +0530

    Implement dealership fetching routes with error handling
    Added error handling and implemented routes to fetch all dealerships and dealerships by state or id.

--- server/database/app.js
+++ server/database/app.js
@@ -52,23 +52,43 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
     const documents = await Reviews.find({dealership: req.params.id});
     res.json(documents);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
+    res.status(500).json({ error: 'Error fetching documents'});
   }
 });
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+	
+  try {
+    const documents = await Dealerships.find({: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents'});
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+
+  try {
+    const document = await Dealerships.find({: req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents'});
+  }
 });
 
 //Express route to insert review
