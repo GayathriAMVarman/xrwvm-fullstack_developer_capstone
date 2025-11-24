13a04b278d3c54bb984daaa1d9f622426edb8ed9
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-11-24 14:19:20 +0530

    Implement get_cars view in views.py
    Added get_cars view to fetch car models and initiate population if no cars exist.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,9 +13,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
-
-
+from .populate import initiate
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -94,3 +92,16 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+# Create a `get_cars` view to render the carmodels of same car make
+# def get_cars(request):
+# ...
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
