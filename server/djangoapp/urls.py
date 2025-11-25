c5f978664e51ce639adb3fa0d65dfa3a4953a9b6
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-11-25 11:42:37 +0530

    Fix syntax error in URL path definition

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,5 +19,5 @@ path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='reviews/dealer/<int:dealer_id>',view = views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
 path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='',view=views.add_review, name='add_review),
+    path(route='',view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
