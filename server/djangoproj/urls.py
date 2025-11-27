02c2a52ba49b1e4c8e77a5e0f00ef7050364979c
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-11-27 14:05:41 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),   path('about', TemplateView.as_view(template_name="About.html")),
     path('contact', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="Login.html")),path('register/', TemplateView.as_view(template_name="Register.html")),
-        path('dealers/', TemplateView.as_view(template_name="Dealer.html")),
+        path('dealers/', TemplateView.as_view(template_name="Dealers.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="Dealer.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
