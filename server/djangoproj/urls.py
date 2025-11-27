fc8d26c7e1a7b1afeda53cc52e8d2c89e9fd684f
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-11-27 15:22:31 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),   path('about', TemplateView.as_view(template_name="About.html")),
     path('contact', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="Login.html")),path('register/', TemplateView.as_view(template_name="Register.html")),
-        path('dealers/', TemplateView.as_view(template_name="Dealers.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="Dealer.html")),
-        path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="Review.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),path('register/', TemplateView.as_view(template_name="index.html")),
+        path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+        path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
