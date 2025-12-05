ee62f7209c7869d3e26d4c42db3d285798e5b5b9
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-12-05 11:54:13 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -128,20 +128,24 @@ def add_review(request):
             # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            return JsonResponse({"status": 401,"message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403,"message": "Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
 
 
 # Create a `get_cars` view to render the carmodels of same car make
 # ...
 def get_cars(request):
-    count = CarMake.objects.filter().count()
+    count = CarMake.objects
+            .filter().count()
     print(count)
     if(count == 0) :
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
