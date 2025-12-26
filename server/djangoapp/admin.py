1e85a5f803b2b3ceb65dbf7f223ddee61441b9db
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2025-12-26 13:07:49 +0530

    Update CarModelInline to use StackedInline
    Changed CarModelInline from TabularInline to StackedInline for better layout.

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,11 +6,10 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 admin.site.register(CarMake)
-admin.site.register(CarModel)  
 # Register your models here.
 
 # CarModelInline class
-class CarModelInline(admin.TabularInline):
+class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 1  # Number of empty forms to show
 
@@ -18,6 +17,6 @@ class CarModelInline(admin.TabularInline):
 class CarModelAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]  # Registering the inline
 
-# CarMakeAdmin class with CarModelInline
+# CarMakeAdmin class with CarModelInline 
 admin.site.register(CarModel, CarModelAdmin)
 # Register models here
