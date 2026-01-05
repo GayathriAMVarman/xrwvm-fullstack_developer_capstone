788c80387cbed78638baa3dc225d43b2594b5261
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2026-01-05 22:11:12 +0530

    Fix indentation in populate.py for data loops

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -11,8 +11,8 @@ def initiate():
     ]
 
     car_make_instances = []
-for data in car_make_data:
-    car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+    for data in car_make_data:
+      car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
 
 
     # Create CarModel instances with 
@@ -36,5 +36,5 @@ for data in car_make_data:
         # Add more CarModel instances as needed
     ]
 
-for data in car_model_data:
-    CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+    for data1 in car_model_data:
+      CarModel.objects.create(name=data1['name'], car_make=data1['car_make'], type=data1['type'], year=data1['year'])
