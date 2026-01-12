430b5a1f7798a64a8615a63a5b98b9a9d8cd3208
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2026-01-12 20:53:47 +0530

    Improve error handling in REST API functions

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url',
+                                   default="http://localhost:5050/")
 
 
 def get_request(endpoint, **kwargs):
@@ -22,9 +23,9 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"{e} exception occurred")
 
 
 def analyze_review_sentiments(text):
@@ -44,5 +45,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"{e} exception occurred")
