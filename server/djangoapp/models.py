15c73398d7e8308ae043cf920ab46e4b5592b8a5
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2026-01-12 20:33:11 +0530

    Refactor comments and formatting in models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,8 +11,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
-# - __str__ method to 
-# print a car make object
+# - __str__ method to print a car make object
 
 
 class CarMake(models.Model):
@@ -33,21 +32,20 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to 
 # print a car make object
-
-
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan') ,
-        ('SUV', 'SUV') ,
-        ('WAGON', 'Wagon') ,
-# Add more choices as required
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,validators=[MaxValueValidator(2023),MinValueValidator(2015)])
-# Other fields as needed
+    year = models.IntegerField(default=2023, 
+                               validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
