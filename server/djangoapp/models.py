471074569be1af50075ce4c0faaeb9b82a0bad1c
GayathriAMVarman <103264236+GayathriAMVarman@users.noreply.github.com>
2026-01-13 11:56:18 +0530

    Update CarModel class with necessary fields
    Added CarModel class with fields for make, type, and year.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -22,6 +22,8 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -30,21 +32,25 @@ class CarMake(models.Model):
 # such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
-# - __str__ method to 
+# - __str__ method to
 # print a car make object
+
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
     # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-    # Add more choices as required
+            ('SEDAN', 'Sedan'),
+            ('SUV', 'SUV'),
+            ('WAGON', 'Wagon'),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(max_length=10,
+                            choices=CAR_TYPES,
+                            default='SUV')
     year = models.IntegerField(default=2023, 
-                               validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+                               validators=[MaxValueValidator(2023),
+                                           MinValueValidator(2015)])
     # Other fields as needed
 
     def __str__(self):
